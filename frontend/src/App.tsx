7580051cd0fd4a5063a4330c00c95fa162f7c17f
DBiles <darron.biles@gmail.com>
2023-04-03 22:09:03 -0400

    Sample react hooks and state being passed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,17 @@ import { Nav } from "./components/Navbar/Navbar";
 import { Game } from "./components/Game/game";
 import ContractCard from "./components/ContractCard/ContractCard";
 
+declare let window: any;
+
 function App() {
+
+  // Contract Address
+  const CONTRACT_ADDRESS: string = "0xD48493103049326ddF73fC704939eb05Ae899e0b";
+
+  
   return (
     <div className="App">
-      <Nav />
+      <Nav contractAddress={CONTRACT_ADDRESS}/>
       <Gameboard />
       <Game />
       <ContractCard />
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
-import { ethers } from "ethers";
+import React from "react";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
 import ConnectButton from "../ConnectButton/ConnectButton";
-import prizeABI from "../../assets/blockchain/Prize.json";
+import './navbar.css'
 
 
-declare let window: any;
-
-export function Nav() {
+export function Nav(props: any) {
 
   // Contract Hook and Address
-  const CONTRACT_ADDRESS = "0xD48493103049326ddF73fC704939eb05Ae899e0b";
+  // const CONTRACT_ADDRESS = "0xD48493103049326ddF73fC704939eb05Ae899e0b";
 
-  // Set ethereum contract
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const prizeContract = new ethers.Contract(CONTRACT_ADDRESS, prizeABI.abi, provider);
 
   return (
     <Container fluid>
       <Stack direction="horizontal" gap={3}>
         <div className="bg-light">Team 2</div>
-        <div className="bg-light ms-auto">
-          <a href="https://mumbai.polygonscan.com/address/0xD48493103049326ddF73fC704939eb05Ae899e0b" target="_blank" rel="noreferrer">
-            {CONTRACT_ADDRESS}
+        <div className="bg-light ms-auto"> Contract Address: 
+          <a
+            href="https://mumbai.polygonscan.com/address/0xD48493103049326ddF73fC704939eb05Ae899e0b"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {props.contractAddress}
           </a>
         </div>
         <div className="bg-light">Third item</div>
